0546b4a67d5f09bc6ce9f220693282fe14ce2cd2
yacynr <yassine.benjaber@esprit.tn>
2023-03-09 09:41:56 +0100

    reglage interface
    reglage interface et controle de saisie

--- src/oxidus/gui/menuReservationController.java
+++ src/oxidus/gui/menuReservationController.java
@@ -42,16 +42,16 @@ import oxidus.services.ServReservation;
  * @author lelou
  */
 public class menuReservationController implements Initializable {
+
     @FXML
     private TableView<Reservation> tabViewReserv;
-    
-    
+
     @FXML
     private Button btn_imprimer;
 
     @FXML
     private TableColumn<Reservation, String> col_nom_res;
-        @FXML
+    @FXML
     private TableColumn<Reservation, String> col_status;
 
     @FXML
@@ -62,25 +62,26 @@ public class menuReservationController implements Initializable {
 
     @FXML
     private TableColumn<Reservation, Integer> col_prix_res;
-    
-     @FXML
+
+    @FXML
     private TableColumn<Reservation, String> col_modele;
-    
-     @FXML
+
+    @FXML
     private AnchorPane anchPie;
-     
+
     @FXML
     private TextField textfiel_recherche;
-    
 
     ServReservation servicesRese = new ServReservation();
-
-   // ObservableList<Reservation> listRes;
       @FXML
+    private Button btn_alleragence;
+
+    // ObservableList<Reservation> listRes;
+    @FXML
     private Button btn_supp_res;
 
-      int myIndex;
-      
+    int myIndex;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
@@ -89,9 +90,25 @@ public class menuReservationController implements Initializable {
     }
 
     public void affichagerTableMenu() {
-       ServReservation svr = new ServReservation();
+        ServReservation svr = new ServReservation();
         afficherReservations(svr.afficherReservation());
 
+    }
+    
+    @FXML
+    void btn_alleragence(ActionEvent event) {
+         try {
+                        // Charger la scène2.fxml
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("ConsulterAgence.fxml"));
+                        Parent root = loader.load();
+
+                        // Afficher la scène2
+                        Scene scene = new Scene(root);
+                        Stage stage = (Stage) btn_alleragence.getScene().getWindow();
+                        stage.setScene(scene);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } 
     }
 
     public void afficherReservations(List<Reservation> reservations) {
@@ -103,8 +120,8 @@ public class menuReservationController implements Initializable {
         col_prix_res.setCellValueFactory(new PropertyValueFactory<Reservation, Integer>("prix"));
         col_modele.setCellValueFactory(new PropertyValueFactory<Reservation, String>("modele"));
         col_status.setCellValueFactory(new PropertyValueFactory<Reservation, String>("status"));
-        
-               //****************** recherche *****************************************
+
+        //****************** recherche *****************************************
         FilteredList<Reservation> filterdata = new FilteredList<Reservation>(listeReservations, btn_supp_res -> true);
 
         //2. set filter predicate whenever rhe filter changes.
@@ -118,20 +135,23 @@ public class menuReservationController implements Initializable {
                 String lowerCaseFilter = newValue.toLowerCase();
                 String prix = "" + reservation.getPrix();
                 String dateDeb = "" + reservation.getDate_debut();
-                String dateFin= "" + reservation.getDate_fin();
+                String dateFin = "" + reservation.getDate_fin();
                 if (reservation.getModele().toLowerCase().indexOf(lowerCaseFilter) != -1) {
                     return true;  //filter match titre
                 }
                 if (reservation.getNom_user().toLowerCase().indexOf(lowerCaseFilter) != -1) {
                     return true;  //filter match type collaboration
-                }if (reservation.getStatus().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                }
+                if (reservation.getStatus().toLowerCase().indexOf(lowerCaseFilter) != -1) {
                     return true;  //filter match type collaboration
                 }
                 if (prix.toLowerCase().indexOf(lowerCaseFilter) != -1) {
                     return true;  //filter match type collaboration
-                } if (dateDeb.toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                }
+                if (dateDeb.toLowerCase().indexOf(lowerCaseFilter) != -1) {
                     return true;  //filter match type collaboration
-                } if (dateFin.toLowerCase().indexOf(lowerCaseFilter) != -1) {
+                }
+                if (dateFin.toLowerCase().indexOf(lowerCaseFilter) != -1) {
                     return true;  //filter match type collaboration
                 } else {
                     return false;
@@ -146,9 +166,7 @@ public class menuReservationController implements Initializable {
         sortedData.comparatorProperty().bind(tabViewReserv.comparatorProperty());
 
         tabViewReserv.setItems(sortedData);
-        
-        
-             
+
         //affichage dynamique
         tabViewReserv.setRowFactory(tabrow -> {
 
@@ -157,23 +175,23 @@ public class menuReservationController implements Initializable {
                     -> {
                 if (event.getClickCount() == 1 && (!myRow.isEmpty())) {
                     myIndex = tabViewReserv.getSelectionModel().getSelectedIndex();
-             /*       id = Integer.parseInt(String.valueOf(tabViewReserv.getItems().get(myIndex).getId_collaboration()));
+                    /*       id = Integer.parseInt(String.valueOf(tabViewReserv.getItems().get(myIndex).getId_collaboration()));
                     label_descrition.setText(tableGeneralCollab.getItems().get(myIndex).getTitre()); */
 
                     Data.modele = tabViewReserv.getItems().get(myIndex).getModele();
                     Data.nom_reser = tabViewReserv.getItems().get(myIndex).getNom_user();
-                 //   String pprix = tabViewReserv.getItems().get(myIndex).getNom_user();
-                //    Data.prix_total = Integer.parseInt(pprix);
+                    //   String pprix = tabViewReserv.getItems().get(myIndex).getNom_user();
+                    //    Data.prix_total = Integer.parseInt(pprix);
                     Reservation rr = new Reservation();
-                     ServReservation svc = new ServReservation();
-                    rr= svc.recherche(Data.nom_reser,Data.modele);
-                     Data.id = rr.getId_reservation();
-                     System.out.println("la reservation recuperer \n"+rr);
-                     Data.email_User = rr.getEmail_user();
-                     Data.dateDebut = rr.getDate_debut();
-                     Data.dateFin = rr.getDate_fin();
+                    ServReservation svc = new ServReservation();
+                    rr = svc.recherche(Data.nom_reser, Data.modele);
+                    Data.id = rr.getId_reservation();
+                    System.out.println("la reservation recuperer \n" + rr);
+                    Data.email_User = rr.getEmail_user();
+                    Data.dateDebut = rr.getDate_debut();
+                    Data.dateFin = rr.getDate_fin();
                     // label_periode.setText(tableGeneralCollab.getItems().get(myIndex).getDescription());
-                /*    label_periode.setText(cc.getDate_sortie().toString());
+                    /*    label_periode.setText(cc.getDate_sortie().toString());
                     label_descrition.setText(cc.getDescription());
                     label_titre.setText(cc.getTitre());
                     label_email.setText(cc.getEmail_user()); */
@@ -182,54 +200,56 @@ public class menuReservationController implements Initializable {
             return myRow;
         });
     }
-    
-    
-      @FXML
+
+    @FXML
     void btn_supp_res(ActionEvent event) {
         Reservation r = new Reservation();
         r.setId_reservation(Data.id);
         r.setStatus(Data.ValeurStatus[3]);
         ServReservation svc = new ServReservation();
-          System.out.println("suppression id = "+Data.id);
-          System.out.println("valeur data status = "+Data.ValeurStatus[3]);
-        if(svc.modifierStatusReservation(r)){
+        System.out.println("suppression id = " + Data.id);
+        System.out.println("valeur data status = " + Data.ValeurStatus[3]);
+        if (svc.modifierStatusReservation(r)) {
             Data.information("Annulation", "Votre Demande d'annulation a été pris en compte");
             ServReservation serrese = new ServReservation();
             //----------- envoie message -----------------
             Email e = new Email();
-                try {
-                    e.envoyer(Data.email_User, LocalDate.now().toString(), r.getNom_user(),"Reservation annuler merci de votre "
-                            + "confiance en vres VROM VROM");
-                } catch (MessagingException ex) {
-                    Logger.getLogger(AjourReservationController.class.getName()).log(Level.SEVERE, null, ex);
-                }
+            try {
+                e.envoyer(Data.email_User, LocalDate.now().toString(), r.getNom_user(), "Reservation annuler merci de votre "
+                        + "confiance en vres VROM VROM");
+            } catch (MessagingException ex) {
+                Logger.getLogger(AjourReservationController.class.getName()).log(Level.SEVERE, null, ex);
+            }
             affichagerTableMenu();
-        }else{
+        } else {
             System.out.println("echec suppression");
         }
     }
-    
-    
-     @FXML
+
+    @FXML
     void btn_imprimer(ActionEvent event) {
-         try {
-                    // Charger la scène2.fxml
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("imprimerRerservation.fxml"));
-                    Parent root = loader.load();
+        if (Data.nom_reser.isEmpty()) {
+            Data.warning("erreur", "veuillez choisir un element a imprimer");
+        } else {
+            try {
+                // Charger la scène2.fxml
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("imprimerRerservation.fxml"));
+                Parent root = loader.load();
 
-                    // Passer les données de nom et prénom à la scène2
-                    ImprimerRerservationController modifController = loader.getController();
-                    modifController.afficherModif(Data.nom_reser, Data.modele, Data.dateDebut, Data.dateFin, Data.prix,
-                            Data.prix_total, Data.nbre_jours);
+                // Passer les données de nom et prénom à la scène2
+                ImprimerRerservationController modifController = loader.getController();
+                modifController.afficherModif(Data.nom_reser, Data.modele, Data.dateDebut, Data.dateFin, Data.prix,
+                        Data.prix_total, Data.nbre_jours);
 
-                    // Afficher la scène2
-                    Scene scene = new Scene(root);
-                    Stage stage = (Stage) btn_imprimer.getScene().getWindow();
-                    stage.setScene(scene);
-                } catch (IOException ee) {
-                    ee.printStackTrace();
-                }
-                
+                // Afficher la scène2
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) btn_imprimer.getScene().getWindow();
+                stage.setScene(scene);
+            } catch (IOException ee) {
+                ee.printStackTrace();
+            }
+        }
+
     }
-    
+
 }
